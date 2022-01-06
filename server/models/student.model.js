dc2f5affc5f7a993d00d77c21debafa93e00e56a
Djalalov <edjalalov@outlook.com>
2022-01-06 17:18:28 +0500

    created student model

---
+++ server/models/student.model.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const studentSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minLength: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minLength: true,
+    },
+    studentID: {
+      type: Number,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    level: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    analysis: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const student = mongoose.model("student", studentSchema);
+module.exports = Student;
--- server/server.js
+++ server/server.js
@@ -1,9 +1,8 @@
-import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
-import dotenv from "dotenv";
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-//require("dotenv").config();
+require("dotenv").config();
 
 const app = express();
 const port = process.env.port || 5000;
@@ -15,7 +14,6 @@ const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
 });
 
 const connection = mongoose.connection;
@@ -23,6 +21,10 @@ const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("MongoDB database has been connected successfully");
 });
+
+const studentRoutes = require("./routes/students");
+
+app.use("/students", studentRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running at ${port}`);
