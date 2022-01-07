05a3d68468f4eb19ea84cd30276eeb056146ee38
Djalalov <edjalalov@outlook.com>
2022-01-07 13:54:52 +0500

    update

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Student Registration Form</h1>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
